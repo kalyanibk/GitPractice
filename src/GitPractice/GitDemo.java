3d1f59bb2e532e61d4a3c6fbadd2c08857d6d0c6
kalyanibk <kalyanikongare1998@gmail.com>
2023-07-14 08:54:37 +0530

    2nd time push code

--- src/GitPractice/GitDemo.java
+++ src/GitPractice/GitDemo.java
@@ -1,6 +1,14 @@
 package GitPractice;
 
 public class GitDemo {
+	
+	
+	//method
+	
+	public void branch() {
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
